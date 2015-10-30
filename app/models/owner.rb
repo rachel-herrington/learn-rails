e61ca3b5a3ee3b6c6dc1d081cca88de7410554e6
Rachel Herrington <rachelhelotes@gmail.com>
2015-10-30 18:03:20 +0000

    update application layout

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,13 +8,11 @@ class Owner
     birthdate = Date.new(1990,12,22)
    end
 
-   def countdown
+  def countdown
      today = Date.today
      birthday = Date.new(today.year, birthdate.month, birthdate.day)
-     if birthday > today
-       countdown = (birthday - today).to_i
-     else
-       countdown = (birthday.next_year - today).to_i
+     return (birthday - today).to_i
+     if birthday > today(birthday.next_year - today).to_i
      end
    end
 end
