d4a34c7fdb0288096eeb4e687b10ef0d5a118d06
Rachel Herrington <rachelhelotes@gmail.com>
2015-10-31 16:50:06 +0000

    front-end framework

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -11,8 +11,7 @@ class Owner
   def countdown
      today = Date.today
      birthday = Date.new(today.year, birthdate.month, birthdate.day)
-     return (birthday - today).to_i
-     if birthday > today(birthday.next_year - today).to_i
-     end
+     return (birthday - today).to_i if birthday > today
+     (birthday.next_year - today).to_i
    end
 end
